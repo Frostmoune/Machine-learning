f063c1f7259021a7a3d04a9802a0a0c16d304526
ouyry <810343087@qq.com>
2018-07-24 10:49:23 +0800

    RandomForest

---
+++ RandomForest.py
@@ -0,0 +1,83 @@
+from Tree import Tree, Node
+import numpy as np 
+import random
+
+class RandomTree(Tree):
+    def __init__(self, all_features, all_labels, feature_len, max_son, max_leaf_data):
+        super(RandomTree, self).__init__(all_features, all_labels, feature_len, max_son, max_leaf_data)
+        self.divide_feature_len = int(np.log2(feature_len)) + 1
+    
+    def buildRandomNode(self, now_root):
+        now_counts = self.getCount(now_root.data_index)
+        if now_counts == None or len(now_counts) == 0:
+            return
+        if len(now_root.data_index) <= self.max_leaf_data or len(now_counts) == 1:
+            now_root.is_leaf = True
+            self.setClassfy(now_root, now_counts)
+            return
+        best_sub_data_indexs = []
+        best_gain = -100000000
+        best_divide_feature_index = -1
+        best_value = ()
+
+        # 随机选择用于划分的特征值的子集
+        divide_list = list(range(self.feature_len))
+        random.shuffle(divide_list)
+        divide_list = divide_list[0:self.divide_feature_len]
+
+        for i in divide_list:
+            now_value, now_sub_data_indexs = self.getSubIndex(now_root.data_index, i)
+            now_gain = self.getGain(now_root.data_index, now_sub_data_indexs)
+            if best_gain < now_gain:
+                best_divide_feature_index = i
+                best_gain = now_gain
+                best_sub_data_indexs = now_sub_data_indexs
+                best_value = now_value
+
+        for i in range(len(best_sub_data_indexs)):
+            new_son = Node(best_sub_data_indexs[i])
+            new_son.setDivideFeatureAndValue(best_divide_feature_index, best_value[0] + best_value[1] * (i + 1))
+            self.buildNode(new_son)
+            now_root.son.append(new_son)
+
+    def train(self):
+        self.buildRandomNode(self.root) 
+
+class RandomForest(object):
+    def __init__(self, trees_num):
+        self.trees_num = trees_num
+        self.trees = [0 for i in range(trees_num)]
+    
+    # 得到每棵树用于训练的数据
+    def getRandomData(self, all_features, all_labels):
+        sub_features = []
+        sub_labels = []
+        for i in range(len(all_features)):
+            if random.random() < 0.500000001:
+                sub_features.append(all_features[i])
+                sub_labels.append(all_labels[i])
+        return sub_features, sub_labels
+    
+    # 建立每棵树
+    def buildTrees(self, all_features, all_labels, feature_len, max_son, max_leaf_data):
+        for i in range(self.trees_num):
+            sub_features, sub_labels = self.getRandomData(all_features, all_labels)
+            self.trees[i] = RandomTree(sub_features, sub_labels, feature_len, max_son, max_leaf_data)
+            self.trees[i].train()
+    
+    # 用随机森林进行预测
+    def predictForest(self, test_feature):
+        res = -1
+        temp = -1
+        now_res = 0
+        count = {}
+        for i in range(self.trees_num):
+            now_res = self.trees[i].predictTree(test_feature)
+            if now_res not in count:
+                count[now_res] = 1
+            else:
+                count[now_res] += 1
+            if temp < count[now_res]:
+                temp = count[now_res]
+                res = now_res
+        return now_res
--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 from KNN import Knn
 from Tree import Tree
+from RandomForest import RandomForest
 from DataProvider import DataProvider
 
+# KNN测试
 def knnTest(feature_len, all_lines, all_features, all_labels):
     for i in range(10):
         rate = 0
@@ -21,7 +23,11 @@ def knnTest(feature_len, all_lines, all_features, all_labels):
             print(rate / length)
         all_features, all_labels = now_provider.getFeatureAndLabel(all_lines, feature_len)
 
+# 决策树测试
 def treeTest(feature_len, all_lines, all_features, all_labels):
+    best_max_son = 0
+    temp = 0
+    counts = {}
     for i in range(10):
         rate = 0
         print("Test %d:"%(i + 1))
@@ -31,6 +37,8 @@ def treeTest(feature_len, all_lines, all_features, all_labels):
         test_labels = all_labels[int(0.8 * len(all_features)):]
         for max_son in range(2, 11):
             rate = 0
+            if max_son not in counts:
+                counts[max_son] = 0
             print("max_son:%d "%(max_son), end = " ")
             new_tree = Tree(train_features, train_labels, len(train_features[0]), max_son, 5)
             new_tree.train()
@@ -39,20 +47,59 @@ def treeTest(feature_len, all_lines, all_features, all_labels):
                 res = new_tree.predictTree(test_features[j])
                 if res == test_labels[j]:
                     rate += 1
-            print(rate / length) 
+            print(rate / length)
+            counts[max_son] += rate / length
+            if temp < counts[max_son]:
+                temp = counts[max_son]
+                best_max_son = max_son 
         all_features, all_labels = now_provider.getFeatureAndLabel(all_lines, feature_len)
+    print("Best best_max_son:%d "%(best_max_son))
+
+# 随机森林测试
+def randomForestTest(feature_len, all_lines, all_features, all_labels):
+    best_trees_num = 0
+    temp = 0
+    counts = {}
+    for i in range(10):
+        rate = 0
+        print("Test %d:"%(i + 1))
+        train_features = all_features[0:int(0.8 * len(all_features))]
+        train_labels = all_labels[0:int(0.8 * len(all_features))]
+        test_features = all_features[int(0.8 * len(all_features)):]
+        test_labels = all_labels[int(0.8 * len(all_features)):]
+        for trees_num in range(5, 26):
+            rate = 0
+            if trees_num not in counts:
+                counts[trees_num] = 0
+            print("trees_num:%d "%(trees_num), end = " ")
+            new_forest = RandomForest(trees_num)
+            new_forest.buildTrees(train_features, train_labels, len(train_features[0]), 3, 5)
+            length = len(test_labels)
+            for j in range(0, length):
+                res = new_forest.predictForest(test_features[j])
+                if res == test_labels[j]:
+                    rate += 1
+            print(rate / length)
+            counts[trees_num] += rate / length
+            if temp < counts[trees_num]:
+                temp = counts[trees_num]
+                best_trees_num = trees_num
+        all_features, all_labels = now_provider.getFeatureAndLabel(all_lines, feature_len)
+    print("Best trees_num:%d "%(best_trees_num))
 
 if __name__ == "__main__":
-    # feature_len = 60
-    # now_provider = DataProvider("https://archive.ics.uci.edu/ml/machine-learning-databases/undocumented/connectionist-bench/sonar/sonar.all-data")
-    # all_lines = now_provider.read(feature_len)
-    feature_len = 4
-    now_provider = DataProvider("http://archive.ics.uci.edu/ml/machine-learning-databases/00267/data_banknote_authentication.txt")
+    feature_len = 60
+    now_provider = DataProvider("https://archive.ics.uci.edu/ml/machine-learning-databases/undocumented/connectionist-bench/sonar/sonar.all-data")
     all_lines = now_provider.read(feature_len)
+    # feature_len = 4
+    # now_provider = DataProvider("http://archive.ics.uci.edu/ml/machine-learning-databases/00267/data_banknote_authentication.txt")
+    # all_lines = now_provider.read(feature_len)
     all_features, all_labels = now_provider.getFeatureAndLabel(all_lines, feature_len)
 
-    cho = '2'
+    cho = '3'
     if cho == '1':
         knnTest(feature_len, all_lines, all_features, all_labels)
     elif cho == '2':
         treeTest(feature_len, all_lines, all_features, all_labels)
+    elif cho == '3':
+        randomForestTest(feature_len, all_lines, all_features, all_labels)
